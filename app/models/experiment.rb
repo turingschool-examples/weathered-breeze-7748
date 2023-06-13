1bb8c65e748a9c3d62b8cbf1456da649fae59432
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-06-13 11:54:27 -0600

    add class method

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -1,4 +1,8 @@
 class Experiment < ApplicationRecord
   has_many :experiments_scientists
   has_many :scientists, through: :experiments_scientists
+
+  def self.long_run_experiments
+    where('num_months >= 6').order(num_months: :desc).pluck(:name)
+  end
 end
