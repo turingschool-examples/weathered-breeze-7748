37095b0cfcc5cce9b7265d8e0aa1124bb2d17f5b
Andy Weissman <andyweissman6@gmail.com>
2023-06-13 12:44:53 -0600

    add model test for #over_six_months

--- spec/models/experiment_spec.rb
+++ spec/models/experiment_spec.rb
@@ -3,4 +3,31 @@ require 'rails_helper'
 RSpec.describe Experiment do
   it {should have_many :scientist_experiments}
   it {should have_many(:scientists).through(:scientist_experiments)}
+
+  describe "class methods" do
+    before(:each) do
+      @lab1 = Lab.create!(name: "710labs")
+      @lab2 = Lab.create!(name: "Dexter's Lab")
+
+      @scientist1 = Scientist.create!(name: "Mike Jones", specialty: "Drank", university: "Houston", lab: @lab1)
+      @scientist2 = Scientist.create!(name: "Sean Paul", specialty: "Dance", university: "South Harmon Institute of Tech", lab: @lab1)
+      @scientist3 = Scientist.create!(name: "Bob Barker", specialty: "Price", university: "CU Boulder", lab: @lab1)
+      @scientist4 = Scientist.create!(name: "Tom Hanks", specialty: "Love", university: "Alabama", lab: @lab2)
+      
+      @experiment1 = Experiment.create!(name: "Mentos and Diet Coke", objective: "Save the Planet", num_months: 420)
+      @experiment2 = Experiment.create!(name: "Baking Soda Volcano", objective: "Educate children", num_months: 69)
+      @experiment3 = Experiment.create!(name: "Project X", objective: "Party", num_months: 5)
+      @experiment4 = Experiment.create!(name: "Wolverine", objective: "Make Claws", num_months: 7)
+    
+      ScientistExperiment.create!(experiment: @experiment1, scientist: @scientist1)
+      ScientistExperiment.create!(experiment: @experiment1, scientist: @scientist2)
+      ScientistExperiment.create!(experiment: @experiment2, scientist: @scientist3)
+      ScientistExperiment.create!(experiment: @experiment3, scientist: @scientist4)
+      ScientistExperiment.create!(experiment: @experiment4, scientist: @scientist1)
+    end 
+  
+    it "#over_six_months" do
+      expect(Experiment.over_six_months).to eq([@experiment1, @experiment2, @experiment4])
+    end
+  end
 end
