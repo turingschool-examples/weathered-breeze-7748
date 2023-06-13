c786a1d2ef205e1dd683feac8a0da4ac704d26f8
Jesse Sorman <jessesorman@gmail.com>
2023-06-13 15:59:07 -0400

    fix routes

--- app/controllers/scientist_experiments_controller.rb
+++ app/controllers/scientist_experiments_controller.rb
@@ -1,7 +1,7 @@
 class ScientistExperimentsController < ApplicationController
   def destroy
     scientist_experiment = ScientistExperiment.find(params[:id])
-    scientist = Scientist.find_by(id: scientist_experiment.scientist_id)
+    scientist = Scientist.find(params[:scientist_id])
     scientist_experiment.destroy
     redirect_to "/scientists/#{scientist.id}"
   end
