c8e26b6c55c509347e6ed7ba1ff8981562d04860
Jesse Sorman <jessesorman@gmail.com>
2023-06-13 13:45:29 -0400

    refactor test data

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -3,6 +3,6 @@ class Experiment < ApplicationRecord
   has_many :scientists, through: :scientist_experiments
 
   def self.longer_than_6
-    where("num_months > ?", 6).distinct.order(num_months: :desc)
+    where("num_months > ?", 6).order(num_months: :desc)
   end
 end
--- spec/features/scientists/show_spec.rb
+++ spec/features/scientists/show_spec.rb
@@ -6,10 +6,14 @@ RSpec.describe "scientists show page" do
     @scientist_1 = @lab_1.scientists.create!(name: "smarty", specialty: "pharma", university: "UOR")
     @scientist_2 = @lab_1.scientists.create!(name: "genius", specialty: "physics", university: "ROR")
     @scientist_3 = @lab_1.scientists.create!(name: "smarty pants", specialty: "chemistry", university: "FOT")
-    @experiment_1 = @scientist_1.experiments.create!(name: "secret project", objective: "classified", num_months: 48)
-    @experiment_2 = @scientist_2.experiments.create!(name: "secret project", objective: "classified", num_months: 48)
-    @experiment_3 = @scientist_1.experiments.create!(name: "project 2", objective: "make cool things", num_months: 20)
-    @experiment_4 = @scientist_2.experiments.create!(name: "project 2", objective: "make cool things", num_months: 20)
+    @experiment_1 = Experiment.create!(name: "secret project", objective: "classified", num_months: 9)
+    @experiment_2 = Experiment.create!(name: "project 2", objective: "make cool things", num_months: 7)
+    @experiment_3 = Experiment.create!(name: "project zeus", objective: "make compound", num_months: 8)
+    @experiment_4 = Experiment.create!(name: "project posedion", objective: "make compounds again", num_months: 10)
+    @science_experiment_1 = ScientistExperiment.create!(scientist: @scientist_1, experiment: @experiment_1)
+    @science_experiment_2 = ScientistExperiment.create!(scientist: @scientist_2, experiment: @experiment_1)
+    @science_experiment_3 = ScientistExperiment.create!(scientist: @scientist_1, experiment: @experiment_2)
+    @science_experiment_4 = ScientistExperiment.create!(scientist: @scientist_3, experiment: @experiment_4)
   end
   it "can display scientist info" do
     visit "/scientists/#{@scientist_1.id}"
