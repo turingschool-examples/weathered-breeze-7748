ccae9cb37d45f2d265de7c77bef6bf886e456e65
Blaine Glasgow <glasgowb@outlook.com>
2023-06-13 09:38:18 -0700

    Add Remove button for experiment on Scientist show page that doesnt effect other scientists show page

--- app/controllers/scientist_experiments_controller.rb
+++ app/controllers/scientist_experiments_controller.rb
@@ -1,5 +1,9 @@
 class ScientistExperimentsController < ApplicationController
 
   def destroy
+    scientist = Scientist.find(params[:id])
+    se = ScientistExperiment.find_by(scientist_id: params[:id], experiment_id: params[:format])
+    se.destroy
+    redirect_to scientist_path(scientist.id)
   end
 end
