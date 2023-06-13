a55e1202c894708d3a2b8871d3da8f95cf985b6b
Jesse Sorman <jessesorman@gmail.com>
2023-06-13 12:06:18 -0400

    US 2 wip

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/scientists/:id", to: "scientists#show"
+  delete "/scientists/:id/scientist_experiments", to: "scientist_experiments#destroy"
 end
--- spec/features/scientists/show_spec.rb
+++ spec/features/scientists/show_spec.rb
@@ -8,7 +8,8 @@ RSpec.describe "scientists show page" do
     @scientist_3 = @lab_1.scientists.create!(name: "smarty pants", specialty: "chemistry", university: "FOT")
     @experiment_1 = @scientist_1.experiments.create!(name: "secret project", objective: "classified", num_months: 48)
     @experiment_2 = @scientist_2.experiments.create!(name: "secret project", objective: "classified", num_months: 48)
-    @experiment_3 = @scientist_3.experiments.create!(name: "secret project", objective: "classified", num_months: 48)
+    @experiment_3 = @scientist_1.experiments.create!(name: "project 2", objective: "make cool things", num_months: 20)
+    @experiment_3 = @scientist_2.experiments.create!(name: "project 2", objective: "make cool things", num_months: 20)
   end
   it "can display scientist info" do
     visit "/scientists/#{@scientist_1.id}"
@@ -16,7 +17,17 @@ RSpec.describe "scientists show page" do
     expect(page).to have_content("Specialty: pharma")
     expect(page).to have_content("University: UOR")
     expect(page).to have_content("Lab: laboratory1")
+    expect(page).to have_content("Experiments: secret project project 2")
+  end
+  it "can remove experiments" do
+    visit "/scientists/#{@scientist_1.id}"
+    expect(page).to have_content("Experiments: secret project project 2")
+    click_button "Remove project 2"
+    expect(current_path).to eq("/scientists/#{@scientist_1.id}")
     expect(page).to have_content("Experiments: secret project")
+    expect(page).to_not have_content("project 2")
+    visit "/scientists/#{@scientist_2.id}"
+    expect(page).to have_content("Experiments: project 2")
 
   end
 end
