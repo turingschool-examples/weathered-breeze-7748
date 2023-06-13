507b067df96972d267e7d157af0d792edf3fffdf
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-06-13 10:42:06 -0600

    add to show actoin

--- app/controllers/scientists_controller.rb
+++ app/controllers/scientists_controller.rb
@@ -2,6 +2,6 @@ class ScientistsController < ApplicationController
   def show
     @scientist = Scientist.find(params[:id])
     @lab = @scientist.lab
-    
+    @experiments = @scientist.experiments
   end
 end
