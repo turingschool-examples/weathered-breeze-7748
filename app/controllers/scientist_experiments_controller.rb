f77b4012f8269a7e03f2307afbb7d3390b84ee6f
Jesse Sorman <jessesorman@gmail.com>
2023-06-13 12:18:05 -0400

    refactor destroy scientists experiment

--- app/controllers/scientist_experiments_controller.rb
+++ app/controllers/scientist_experiments_controller.rb
@@ -1,7 +1,7 @@
 class ScientistExperimentsController < ApplicationController
   def destroy
-    scientist_experiment = ScientistExperiment.destroy(experiment_id: params[:experiment_id], scientist_id: params[:id])
-    scientist = Scientist.find_by(id: scientist_experiment.scientist_id)
+    scientist_experiment = ScientistExperiment.find(params[:id])
+    scientist_experiment.destroy
     redirect_to "/scientists/#{scientist.id}"
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+@lab_1 = Lab.create!(name: "laboratory1")
+    @scientist_1 = @lab_1.scientists.create!(name: "smarty", specialty: "pharma", university: "UOR")
+    @scientist_2 = @lab_1.scientists.create!(name: "genius", specialty: "physics", university: "ROR")
+    @scientist_3 = @lab_1.scientists.create!(name: "smarty pants", specialty: "chemistry", university: "FOT")
+    @experiment_1 = @scientist_1.experiments.create!(name: "secret project", objective: "classified", num_months: 48)
+    @experiment_2 = @scientist_2.experiments.create!(name: "secret project", objective: "classified", num_months: 48)
+    @experiment_3 = @scientist_1.experiments.create!(name: "project 2", objective: "make cool things", num_months: 20)
+    @experiment_3 = @scientist_2.experiments.create!(name: "project 2", objective: "make cool things", num_months: 20)
