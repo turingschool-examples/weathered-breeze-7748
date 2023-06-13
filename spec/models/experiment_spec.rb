f2d75ff35b9f7759778a76bd85071ae0f8c5db91
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-06-13 11:48:37 -0600

    add class method

--- spec/models/experiment_spec.rb
+++ spec/models/experiment_spec.rb
@@ -1,6 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Experiment, type: :model do
-  it {should have_many :experiments_scientists}
-  it {should have_many(:scientists).through(:experiments_scientists)}
+  describe "relationships" do
+    it {should have_many :experiments_scientists}
+    it {should have_many(:scientists).through(:experiments_scientists)}
+  end
+
+
+  describe "class methods" do
+    before(:each) do
+      @lab1 = Lab.create!(name: "The Lab")
+      @lab2 = Lab.create!(name: "Lab 2")
+      @scientist1 = Scientist.create!(name: "Eddie", specialty: "frogs", university: "The University of Colorado", lab_id: @lab1.id)
+      @scientist2 = Scientist.create!(name: "jim", specialty: "worms", university: "Indiana University", lab_id: @lab1.id)
+      @scientist3 = Scientist.create!(name: "bobby", specialty: "snakes", university: "CSU", lab_id: @lab1.id)
+      @scientist4 = Scientist.create!(name: "skeeter", specialty: "dogs", university: "UT", lab_id: @lab2.id)
+      @experiment1 = Experiment.create!(name: "ex 1", objective: "fun", num_months: 3)
+      @experiment2 = Experiment.create!(name: "2", objective: "fun", num_months: 7)
+      @experiment3 = Experiment.create!(name: "ex 3", objective: "fun", num_months: 22)
+      @experiment4 = Experiment.create!(name: "ex 4", objective: "some", num_months: 5)
+      @experiment5 = Experiment.create!(name: "ex 5", objective: "you", num_months: 8)
+      @experiment6 = Experiment.create!(name: "ex 6", objective: "them", num_months: 48)
+      @exp_sci1 = ExperimentsScientist.create!(experiment_id: @experiment1.id, scientist_id: @scientist1.id)
+      @exp_sci2 = ExperimentsScientist.create!(experiment_id: @experiment2.id, scientist_id: @scientist2.id)
+      @exp_sci3 = ExperimentsScientist.create!(experiment_id: @experiment3.id, scientist_id: @scientist3.id)
+      @exp_sci4 = ExperimentsScientist.create!(experiment_id: @experiment4.id, scientist_id: @scientist4.id)
+      @exp_sci5 = ExperimentsScientist.create!(experiment_id: @experiment5.id, scientist_id: @scientist1.id)
+      @exp_sci6 = ExperimentsScientist.create!(experiment_id: @experiment6.id, scientist_id: @scientist2.id)
+    end
+    
+    it "#long_run_experiments" do
+      results = Experiment.long_run_experiments
+      expect(results).to eq([@experiment6.name, @experiment3.name, @experiment5.name, @experiment2.name])
+    end
+  end
 end
